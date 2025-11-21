b393030e278bb3be12082d0293a07b66c8e13d3f
AryanPendke <acppendke@gmail.com>
2025-11-22 04:27:47 +0530

    completed userServices.js

--- trello-lite-backend/src/config/index.js
+++ trello-lite-backend/src/config/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-    port: process.env.port || 3000,
+    port: process.env.port || 5000,
     nodeEnv: process.env.NODE_ENV || 'development',
     logLevel: process.env.LOG_LEVEL || 'info',
 };
